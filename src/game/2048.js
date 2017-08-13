27ce123d8f13872470f527f14edaa097b6845270
tianshi <tianshi@chuangjia.me>
2017-08-13 12:34:18 +0800

    modify 2048 1 remove timeout 2 remove transitionend

--- src/game/2048.js
+++ src/game/2048.js
@@ -9,6 +9,7 @@ export default class Game2048 {
 
         this.oldPos = [];
         this.ePos = [];  // empty value postion
+        this.queue = [];
 
         this.opts = Object.assign({
             box: '.box-2048',
@@ -41,6 +42,7 @@ export default class Game2048 {
         for (let i = 0; i < 2; i++) {
             this.setEmptyValue();
         }
+        this.initAnimation();
     }
 
     initArr() {
@@ -78,7 +80,7 @@ export default class Game2048 {
             disX = event.changedTouches[0].clientX - x1;
             disY = event.changedTouches[0].clientY - y1;
             if (Math.abs(disX) > 10 || Math.abs(disY) > 10) {
-                if (Math.abs(disX) > Math.abs(disY)) {                    
+                if (Math.abs(disX) > Math.abs(disY)) {
                     this.move(this.arr, disX > 0 ? true : false);
                 } else {
                     this.move(this.getV2HArr(this.arr), disY > 0 ? true : false, true);
@@ -93,7 +95,7 @@ export default class Game2048 {
         }
         arr1.sort();
         arr2.sort();
-        for (let i = 0,len = arr1.length;i < len; i++) {
+        for (let i = 0, len = arr1.length; i < len; i++) {
             if (arr1[i] != arr2[i]) {
                 return false;
             }
@@ -105,26 +107,72 @@ export default class Game2048 {
         let arr = this.arr;
         for (let i = 0, len = arr.length; i < len; i++) {
             for (let j = 0; j < len; j++) {
-                if (j != 0 && arr[i][j].value == arr[i][j-1].value) {
+                if (j != 0 && arr[i][j].value == arr[i][j - 1].value) {
                     return false;
                 }
-                if (j < len - 1 && arr[i][j].value == arr[i][j+1].value) {
+                if (j < len - 1 && arr[i][j].value == arr[i][j + 1].value) {
                     return false;
                 }
-                if (i != 0 && arr[i][j].value == arr[i-1][j].value) {
+                if (i != 0 && arr[i][j].value == arr[i - 1][j].value) {
                     return false;
                 }
-                if (i < len - 1 && arr[i][j].value == arr[i+1][j].value) {
+                if (i < len - 1 && arr[i][j].value == arr[i + 1][j].value) {
                     return false;
                 }
             }
         }
+        this.gameOver = true;
         return true;
     }
 
-    setEmptyValue() {
+    initAnimation(temp, time) {
+        if (this.gameOver) {
+            return;
+        }
+        time = time || Date.now();
+        if (temp && (Date.now() - time < this.opts.time)) {
+            this.noMove = true;
+        } else {
+            this.noMove = false;
+            if (Array.isArray(temp)) {
+                temp.forEach(v => {
+                    v.el.style.transition = '';
+                    v.el.style.transform = '';
+                    v.item.value = v.item.newValue;
+                    v.item.num = v.item.newValue = 0;
+                    if (v.item.isMerge) {
+                        v.el.style.animation = `scaleIn ${this.opts.time}ms ease-in`;
+                        v.item.isMerge = false;
+                        function animationhandle() {
+                            this.style.animation = '';
+                            this.removeEventListener('animationend', animationhandle);
+                        }
+                        v.el.addEventListener('animationend', animationhandle);
+                    }
+                })
+            } else if (typeof temp == 'object' && temp.el) {
+                temp.el.style.animation = '';
+            }
+            temp = this.queue.shift();
+            if (temp) {
+                if (Array.isArray(temp)) {
+                    temp.forEach(v => {
+                        v.el.style[v.key] = v.value;
+                    })
+                } else {
+                    temp.cb && temp.cb();
+                }
+                time = Date.now();
+            }
+        }
+        requestAnimationFrame(() => {
+            this.initAnimation(temp, time);
+        })
+    }
+
+    setEmptyValue(animateObj) {
         if (!this.ePos.length) {
-            if (this.isGameOver()) {                
+            if (this.isGameOver()) {
                 alert('game over');
             }
             return;
@@ -141,19 +189,21 @@ export default class Game2048 {
         item.value = this.opts.min;
         if (this.els) {
             let el = this.els[row * this.arr.length + col];
+            animateObj && (animateObj.el = el);
             el.style.animation = `fadeIn ${this.opts.time}ms`;
-            function handle() {
-                this.style.animation = '';
-                this.removeEventListener('animationend', handle);
-            }
-            el.addEventListener('animationend', handle);
         }
         this.ePos.splice(i, 1);
     }
 
     // 往左滑正，往右反；往上滑正，往下滑反。
     move(wholeArr, reverse, isY) {
-        let arr = [];
+        if (this.noMove) {
+            return;
+        }
+        let arr = [], transArr = [], animateObj = {};
+        transArr.indexs = [];
+        transArr.count = 0;
+
         this.oldPos = this.ePos.map(item => item);
         this.ePos.length = 0;
         this.itemW = this.getItemW();
@@ -162,10 +212,10 @@ export default class Game2048 {
         }
         for (let i = 0, len = wholeArr.length; i < len; i++) {
             arr = wholeArr[i];
-            this.computeRowArr(arr, i, reverse, isY);
+            this.computeRowArr(arr, i, reverse, isY, transArr);
             arr.forEach((v, j) => {
                 if (!v.newValue) {
-                    if (!isY) {                        
+                    if (!isY) {
                         this.ePos.push(`${i},${j}`);
                     } else {
                         this.ePos.push(`${j},${i}`);
@@ -173,9 +223,13 @@ export default class Game2048 {
                 }
             })
         }
-        setTimeout(() => {
-            this.setEmptyValue();
-        }, this.opts.time * 2)
+        this.queue.push(transArr);
+        animateObj = {
+            name: 'setEmptyValue', cb: () => {
+                this.setEmptyValue(animateObj);
+            }
+        }
+        this.queue.push(animateObj);
     }
 
     getV2HArr(arr) {
@@ -184,14 +238,14 @@ export default class Game2048 {
             if (!newArr[col]) {
                 newArr[col] = [];
             }
-            for (let row = 0, len = arr.length; row < len; row++) {                
+            for (let row = 0, len = arr.length; row < len; row++) {
                 newArr[col].push(arr[row][col]);
             }
         }
         return newArr;
     }
 
-    computeRowArr(arr, row, reverse, isY) {
+    computeRowArr(arr, row, reverse, isY, transArr) {
         let temp = arr.map(v => {
             return v.value;
         })
@@ -202,7 +256,7 @@ export default class Game2048 {
         temp.forEach((v, i) => {
             arr[i].newValue = v;
             arr[i].transDis = reverse ? arr[i].num * this.itemW : -arr[i].num * this.itemW;
-            this.setAnimation(arr[i], isY ? i * this.arr.length + row : row * this.arr.length + i, isY);
+            this.setAnimation(arr[i], isY ? i * this.arr.length + row : row * this.arr.length + i, isY, transArr);
         })
     }
 
@@ -213,11 +267,11 @@ export default class Game2048 {
             //每个元素应该移动的位置个数
             num = 0,
             arr = arrSrc;
-            
+
         if (isReverse) {
             temp.reverse();
             arr = arrSrc.map(v => {
-                return {value: v.value}
+                return { value: v.value }
             })
             arr.reverse();
         }
@@ -262,33 +316,14 @@ export default class Game2048 {
         }
     }
 
-    setAnimation(item, index, isY) {
+    setAnimation(item, index, isY, transArr) {
         if (!(item.value || item.newValue)) {
             return;
         }
         let self = this;
         let opts = this.opts;
         this.els[index].style.transition = `transform ${opts.time}ms`;
-        function handle() {
-            this.style.transition = '';
-            this.style.transform = '';
-            item.value = item.newValue;
-            item.num = item.newValue = 0;
-            if (item.isMerge) {
-                this.style.animation = `scaleIn ${opts.time}ms ease-in`;
-                item.isMerge = false;
-                function animationhandle() {
-                    this.style.animation = '';
-                    this.removeEventListener('animationend', animationhandle);
-                }
-                this.addEventListener('animationend', animationhandle);
-            }
-            this.removeEventListener('transitionend', handle);
-        }
-        this.els[index].addEventListener('transitionend', handle)
-        requestAnimationFrame(() => {
-            this.els[index].style.transform = `${isY ? 'translateY' : 'translateX'}(${item.transDis}px)`;
-        })
+        transArr.push({ el: this.els[index], item: item, key: 'transform', value: `${isY ? 'translateY' : 'translateX'}(${item.transDis}px)` })
     }
 
     // 判断是否所有元素的transition都结束了
